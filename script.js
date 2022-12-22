523c38b766afcdc49e310cad79a0f9253acc1073
Ranjan Alok <103888844+ranjanalok961@users.noreply.github.com>
2022-12-23 01:01:32 +0530

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+(function(){
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (/* fill in parts of the 'for' loop to loop over names array */ var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (/* fill in condition here */ firstLetter == 'j') {
+      // byeSpeaker.xxxx
+      byeSpeaker.speak(names[i]);
+    } else {
+      // helloSpeaker.xxxx
+      helloSpeaker.speak(names[i]);
+    }
+  }
+  })();
